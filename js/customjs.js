e59edb24a189ad07df7d1abd66f554f6305900e7
pawan <pawankumar.s@karmanya.co.in>
2016-04-14 15:58:27 +0530

    deleted the folder

--- js/customjs.js
+++ js/customjs.js
@@ -1,23 +1,23 @@
 
 $(document).ready(function () {
-    
+
     $("#userLogIn").hide();
-     $("#adminLogIn").hide();
+    $("#adminLogIn").hide();
     $("#hardware").hide();
     $("#software").hide();
-     $("#network").hide();
-     
-     $("#userLogInLi").click(function(){
+    $("#network").hide();
+
+    $("#userLogInLi").click(function () {
         $("#userLogIn").show();
         $("#adminLogIn").hide();
-     });
-     $("#adminLogInLi").click(function(){
+    });
+    $("#adminLogInLi").click(function () {
         $("#userLogIn").hide();
-        $("#adminLogIn").show(); 
-     });
- 
+        $("#adminLogIn").show();
+    });
+
     $("#emailAddress").on("keyup focusout", function () {
-       
+
         var stringOfEmail = $("#emailAddress").val();
         var lengthOfEmail = stringOfEmail.length;
         var indexOfAt = stringOfEmail.indexOf("@");
@@ -44,13 +44,13 @@ $(document).ready(function () {
                 if ((indexofDot - indexOfAt) == 1) {
                     errorInEmail = 1;
                     $("#emailpara").html("No characters between @ and .");
-                     this.setCustomValidity("No characters between @ and .");
+                    this.setCustomValidity("No characters between @ and .");
                     $("#emailAddress").css("border", "1px solid red");
                 }
                 if (indexOfAt < 5) {
                     errorInEmail = 1;
-                    
-                     this.setCustomValidity("Atleast 5 characters before @");
+
+                    this.setCustomValidity("Atleast 5 characters before @");
                     $("#emailpara").html("Atleast 5 characters before @");
                     $("#emailAddress").css("border", "1px solid red");
                 }
@@ -59,21 +59,21 @@ $(document).ready(function () {
             } else {
                 errorInEmail = 1;
                 $("#emailpara").html("Email doesnot contain @ and . ");
-                
-                     $("#emailAddress")[0].setCustomValidity("Email doesnot contain @ and . ");
+
+                $("#emailAddress")[0].setCustomValidity("Email doesnot contain @ and . ");
                 $("#emailAddress").css("border", "1px solid red");
             }
 
         } else {
             errorInEmail = 1;
             $("#emailpara").html("Please enter the Email address");
-            
-                      $("#emailAddress")[0].setCustomValidity("Please enter the Email address");
+
+            $("#emailAddress")[0].setCustomValidity("Please enter the Email address");
             $("#emailAddress").css("border", "1px solid red");
         }
         if (errorInEmail == 0) {
-            
-                     this.setCustomValidity("");
+
+            this.setCustomValidity("");
             $("#emailpara").html('');
             $("#emailAddress").css("border", "");
 
@@ -84,7 +84,7 @@ $(document).ready(function () {
 //First name validation
 
     $("#firstName").on("keyup focusout", function () {
-        
+
         var stringOfName = $("#firstName").val();
         var lengthOfFirstName = stringOfName.length;
         var firstNameValid = checkingNumber(stringOfName);
@@ -98,13 +98,13 @@ $(document).ready(function () {
         }
         if (firstNameValid == 0) {
             errorInName = 1;
-            
+
             this.setCustomValidity("Please enter alphabets");
             $("#firstnamepara").html("Please enter alphabets");
             $("#firstName").css("border", "1px solid red");
         }
         if (errorInName == 0) {
-            
+
             this.setCustomValidity("");
             $("#firstnamepara").html('');
             $("#firstName").css("border", "");
@@ -120,7 +120,7 @@ $(document).ready(function () {
         var errorInLastName = 0;
         if (lengthOfLastName <= 0) {
             errorInLastName = 1;
-            
+
             this.setCustomValidity("Please Enter The Last Name");
             $("#lastnamepara").html("Please enter the Last name");
             $("#lastName").css("border", "1px solid red");
@@ -129,14 +129,14 @@ $(document).ready(function () {
         if (lastNameValid == 0) {
 
             errorInLastName = 1;
-            
+
             this.setCustomValidity("Please enter alphabets");
             $("#lastnamepara").html("Please enter alphabets");
             $("#lastName").css("border", "1px solid red");
         }
 
         if (errorInLastName == 0) {
-            
+
             this.setCustomValidity("");
             $("#lastnamepara").html('');
             $("#lastName").css("border", "");
@@ -154,34 +154,34 @@ $(document).ready(function () {
         if (phoneLength < 10)
         {
             errorphone = 1;
-            
+
             this.setCustomValidity("Enter complete Number");
             $("#mobilepara").html("Enter complete number");
             $("#mobileNumber").css("border", "1px solid red");
         }
         if (phoneLength == 10) {
-            
+
             this.setCustomValidity("");
             $("#mobileNumber").css("border", "");
             $("#mobilepara").html("");
 
         }
 
-        var phoneNoValid = charvalidforphone(phoneValue);
+        var phoneNoValid = charValidForPhone(phoneValue);
 
         if (phoneLength == 10) {
             if (!phoneNoValid)
             {
                 errorphone = 1;
-                
-            this.setCustomValidity("Please Enter numbers");
+
+                this.setCustomValidity("Please Enter numbers");
                 $("#mobilepara").html("Please Enter numbers");
                 $("#mobileNumber").css("border", "1px solid red");
             }
 
             if (phoneNoValid > 0) {
-                
-            this.setCustomValidity("");
+
+                this.setCustomValidity("");
                 $("#mobilepara").html("");
                 $("#mobileNumber").css("border", "");
 
@@ -189,7 +189,7 @@ $(document).ready(function () {
         }
 
         if (errorphone == 0) {
-            
+
             this.setCustomValidity("");
             $("#mobileNumber").css("border", "");
             $("#mobilepara").html("");
@@ -197,28 +197,27 @@ $(document).ready(function () {
 
     });
     $("#zipcode").on("keyup focusout", function () {
-        var errorzip = 0;
-        var zipvalue = $("#zipcode").val();
-        var validzip = charvalidforphone(zipvalue);
-        if(zipvalue.length==6){
-        if (!validzip) {
-            errorzip = 1;
-            $("#zipcodepara").html("Enter zipcode numbers");
-            this.setCustomValidity("Enter zipcode numbers");
-            $("#zipcode").css("border", "1px solid red");
-        }
-    }
-    else{
-        
-            errorzip = 1;
+        var errorZip = 0;
+        var zipValue = $("#zipcode").val();
+        var validZip = charValidForPhone(zipValue);
+        if (zipValue.length == 6) {
+            if (!validZip) {
+                errorZip = 1;
+                $("#zipcodepara").html("Enter zipcode numbers");
+                this.setCustomValidity("Enter zipcode numbers");
+                $("#zipcode").css("border", "1px solid red");
+            }
+        } else {
+
+            errorZip = 1;
             $("#zipcodepara").html("Enter complete zipcode");
             this.setCustomValidity("Enter complete zipcode");
             $("#zipcode").css("border", "1px solid red");
-        
-    }
-        if (errorzip == 0) {
-            
-                     this.setCustomValidity("");
+
+        }
+        if (errorZip == 0) {
+
+            this.setCustomValidity("");
             $("#zipcodepara").html("");
             $("#zipcode").css("border", "");
         }
@@ -227,19 +226,19 @@ $(document).ready(function () {
 
     $("#country").on("keyup focusout", function () {
 
-        var errorcountry = 0;
-        var countryvalue = $("#country").val();
-        var validcountry = checkingNumber(countryvalue);
-        if (!validcountry) {
-            errorcountry = 1;
-            
-                     this.setCustomValidity("Enter valid name");
+        var errorCountry = 0;
+        var countryValue = $("#country").val();
+        var validCountry = checkingNumber(countryValue);
+        if (!validCountry) {
+            errorCountry = 1;
+
+            this.setCustomValidity("Enter valid name");
             $("#countrypara").html("Enter Valid name");
             $("#country").css("border", "1px solid red");
         }
-        if (errorcountry == 0) {
-            
-                     this.setCustomValidity("");
+        if (errorCountry == 0) {
+
+            this.setCustomValidity("");
             $("#countrypara").html("");
             $("#country").css("border", "");
         }
@@ -248,15 +247,15 @@ $(document).ready(function () {
 
 
     $("#city").on("keyup focusout", function () {
-        var errorcity = 0;
-        var cityvalue = $("#city").val();
-        var validcity = checkingNumber(cityvalue);
-        if (!validcity) {
-            errorcity = 1;
+        var errorCity = 0;
+        var cityValue = $("#city").val();
+        var validCity = checkingNumber(cityValue);
+        if (!validCity) {
+            errorCity = 1;
             $("#citypara").html("Enter Valid name");
             $("#city").css("border", "1px solid red");
         }
-        if (errorcity == 0) {
+        if (errorCity == 0) {
 
             $("#citypara").html("");
             $("#city").css("border", "");
@@ -266,39 +265,38 @@ $(document).ready(function () {
 
 
     $("#state").on("keyup focusout", function () {
-        var errorstate = 0;
-        var statevalue = $("#state").val();
-        var validstate = checkingNumber(statevalue);
-        if (!validstate) {
-            errorstate = 1;
+        var errorState = 0;
+        var stateValue = $("#state").val();
+        var validState = checkingNumber(stateValue);
+        if (!validState) {
+            errorState = 1;
             $("#statepara").html("Enter Valid name");
             $("#state").css("border", "1px solid red");
         }
-        if (errorstate == 0) {
+        if (errorState == 0) {
 
             $("#statepara").html("");
             $("#state").css("border", "");
         }
     });
 
-    $("#confirmPassword").on("focusout keyup", function(){
-        
-        var password=$("#password").val();
-        var confirmPassword=$("#confirmPassword").val();
-        if(password!=confirmPassword){
+    $("#confirmPassword").on("focusout keyup", function () {
+
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
+        if (password != confirmPassword) {
             $("#confirmpasswordpara").html("Password Not Matching");
             this.setCustomValidity("Password Not Matching");
             $("#confirmPassword").css("border", "1px solid red");
             $("#password").css("border", "1px solid red");
-        
-        }
-        else{
+
+        } else {
             $("#confirmpasswordpara").html("");
             this.setCustomValidity("");
             $("#password").css("border", "");
             $("#confirmPassword").css("border", "");
         }
-        
+
     });
 
 
@@ -306,16 +304,16 @@ $(document).ready(function () {
 
     function checkingNumber(stringname) {
         var i;
-        var returnvalue;
-        var number1;
+        var returnValue;
+        var numberValidation;
         var lengthof = stringname.length;
         for (i = 0; i < lengthof; i++) {
-            number1 = Number(stringname.charAt(i));
+            numberValidation = Number(stringname.charAt(i));
 
-            if (!number1) {
-                returnvalue = true;
+            if (!numberValidation) {
+                returnValue = true;
             } else {
-                return returnvalue = false;
+                return returnValue = false;
 
             }
         }
@@ -326,17 +324,17 @@ $(document).ready(function () {
 
 
 
-    function charvalidforphone(stringnamephone) {
+    function charValidForPhone(stringNamePhone) {
         var j;
-        var number123;
-        var lengthofphone = stringnamephone.length;
+        var numberLength;
+        var lengthOfPhone = stringNamePhone.length;
 
-        for (j = 0; j < lengthofphone; j++) {
-            number123 = Number(stringnamephone.charAt(j));
+        for (j = 0; j < lengthOfPhone; j++) {
+            numberLength = Number(stringNamePhone.charAt(j));
 
-            if (!number123) {
+            if (!numberLength) {
 
-                if (stringnamephone.charAt(j) == "0")
+                if (stringNamePhone.charAt(j) == "0")
                 {
 
 
